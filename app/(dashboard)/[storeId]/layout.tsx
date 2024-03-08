a70b3fffa1fdcc1fd67a567e94674bafc09f6fbd
Yuki Ogawa <ogachan6464@gmail.com>
2024-03-08 14:04:33 -0700

    create nav bar and store-switcher to select store

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -27,7 +28,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This is navigation bar</div>
+      <Navbar />
       { children }
     </>
   )
