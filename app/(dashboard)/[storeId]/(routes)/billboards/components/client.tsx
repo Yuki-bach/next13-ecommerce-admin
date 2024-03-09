aba12f216357577eb664601b7bc7d435ccbb077c
Yuki Ogawa <ogachan6464@gmail.com>
2024-03-09 13:06:13 -0700

    billboard - data table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,8 +5,19 @@ import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
 
-export const BillboardClient = () => {
+import { BillboardColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
   const router = useRouter();
   const params = useParams();
 
@@ -14,14 +25,22 @@ export const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards 0"
-          description="Create and manage billboards for your store."
+          title={`Billboards: (${data.length})`}
+          description="Manage billboards for your store."
         />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading
+        title="API"
+        description="api calls for billboards."
+      />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId"/>
     </>
   );
 }
